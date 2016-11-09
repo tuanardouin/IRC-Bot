01c755aaec96d31889af933732f4015f6dae4ccc
enerdhil <enerdhil@bob.lan>
2016-11-09 16:08:07 +0100

    Fix(zoulette)

--- modules/zoulette/zoulette.py
+++ modules/zoulette/zoulette.py
@@ -26,16 +26,18 @@ class Zoulette:
                 self.votes.append(infos["fullUser"])
                 self.zouletteName = infos['args'][0]
                 Morphux.sendMessage("You just launched a zoulette against " + infos['args'][0], infos['nick'])
+            else:
+                Morphux.sendMessage("User '" + infos['args'][0] + "' does not exists" , infos['nick'])
         else:
             self.zouletteCount += 1
             if (infos['fullUser'] not in self.votes):
                 Morphux.sendMessage("You just voted against " + self.zouletteName, infos['nick'])
-                self.votes.append(infos["fullUser"])
+                self.votes.append(infos["fullUser"].split("~")[1])
             else:
                 Morphux.sendMessage("You can't vote twice little filou !", infos['nick'])
             if (self.zouletteCount > len(Morphux.currentUsers) / 2):
+                Morphux.kick(self.zouletteName, "We call it democracy.")
                 self.zouletteLaunched = 0
                 self.zouletteCount = 0
                 self.zouletteName = ""
                 self.votes = []
-                Morphux.kick(self.zouletteName, "We call it democracy.")
